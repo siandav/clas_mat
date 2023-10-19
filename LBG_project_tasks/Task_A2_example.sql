c555f405755c823e42a1755f17975600c39db9af
sian d <71644535+siandav@users.noreply.github.com>
2023-10-19 14:14:17 +0100

    Add files via upload

---
+++ LBG_project_tasks/Task_A2_example.sql
@@ -0,0 +1,8 @@
+--Task 2 – query the database to discover the contents 
+
+--1) how many rows are in the Seattle_cycles_station table?
+select * from Seattle_cycles_station
+
+--2) how many unique station IDs are there?
+select ...
+
