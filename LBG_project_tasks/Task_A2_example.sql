00761b571ef25d4d8f42d08d34b8cf969ef01bee
sian d <71644535+siandav@users.noreply.github.com>
2024-03-16 14:36:42 +0000

    pull off extra class mat to work backup

--- LBG_project_tasks/Task_A2_example.sql
+++
@@ -1,8 +0,0 @@
---Task 2 – query the database to discover the contents 
-
---1) how many rows are in the Seattle_cycles_station table?
-select * from Seattle_cycles_station
-
---2) how many unique station IDs are there?
-select ...
-
--- LBG_project_tasks/readme.md
+++
@@ -1,4 +0,0 @@
-### Content 
-
-- Task A2 example SQl file 
-- Task D1-D6 jupyter notebooks
